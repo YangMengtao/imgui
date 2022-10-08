7c8253dc9a2c599c967bae3c92f38e655dabbc2c
yangmengtao <yangmengtao@xd.com>
2022-10-08 18:51:44 +0800

    rename

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
-project "imgui"
+project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    cppdialect "C++17"
     staticruntime "on"
 
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
@@ -17,17 +18,16 @@ project "imgui"
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "imgui_demo.cpp"
+        "imgui_demo.cpp",
     }
     
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
         
-    filter "configurations:Debug"
+    --[[filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "on"
+		optimize "on"--]]
